29824c7a010457c238b91b8dc3479713ffaf290d
giovbruno <giovanni.bruno@gmx.com>
2025-06-14 14:08:21 +0200

    Setting for injection tests

---
+++ fladerunner.py
@@ -0,0 +1,217 @@
+# Routines to run the code and produce tables of parameter results
+
+import os
+import sys
+sys.path.append('../fladerunner/')
+import copy
+import lc_class
+import pickle
+from astropy.io import fits
+from astropy import units, constants
+import numpy as np
+from numpy.polynomial import Polynomial
+import matplotlib
+import matplotlib.pyplot as plt
+import lmfit
+from matplotlib.ticker import ScalarFormatter
+import flare_detection as fd
+import pandas as pd
+import powerlaw
+from scipy import stats
+import seaborn as sns
+from sklearn import linear_model
+homedir = os.path.expanduser('~')
+from pdb import set_trace
+import glob
+
+datadir = '/home/giovanni/Projects/data'
+throughput_folder = datadir + '/filters/'
+
+def call_LC(LC, saveresfolder):
+
+    saveresfile = saveresfolder + LC.split('/')[-1]
+    fout_str = saveresfile.replace('.fits', '_results.pic')
+    if os.path.exists(fout_str):
+        print('LC:', fout_str + ' - already analyzed.')
+        return
+
+    print('LC:', LC)
+    wth, fth = np.loadtxt(throughput_folder + 'TESS_TESS.Red.dat', unpack=True)
+    flarespar, flaresflag = analyse_LC(LC, saveresfolder, wth*units.AA, fth)
+
+    fout = open(fout_str, 'wb')
+    pickle.dump(flarespar, fout)
+    fout.close()
+
+    return
+
+def analyse_LC(LC, saveresfolder, wth, fth, flare_threshold=4., rebin=0):
+    '''
+    Look for flares and dips on a single LC.
+    '''
+
+    flarespar, flaresflag = fd.find_flares(LC, wth=wth, fth=fth, \
+        flare_threshold=flare_threshold, flatten=True, plot_flat=False, \
+        saveplots=saveresfolder, normalise=True, \
+        fit_continuum=1, complexity=5, clip=True, \
+        rebin=rebin, verbose=True, filt_kernel_size=11)
+
+    return flarespar, flaresflag
+
+def get_results(LCsample, result_path, get_lcs_without_flares=False, \
+                get_csv=True):
+    '''
+    Produce a pandas DataFrame with parameter results from a set of result
+    files.
+    '''
+
+    wth, fth = np.loadtxt(throughput_folder + 'TESS_TESS.Red.dat', unpack=True)
+
+    results = {}
+    parameters = ['LCname', 'Peak amplitude', 'Peak luminosity [erg s$^{-1}$]', \
+        'Duration [min]', 'FWHM [min]', 'Energy (Shibayama) [erg]', \
+        'Energy (Davenport) [erg]', 'ED [s]', 'Peak time', 'n_event', \
+        'total_duration', 'Teff [K]', 'logg', 'radius', \
+        'phot_var', 'tessmag', 'Prot', 'FAP', 'redchi2', 'ticname', \
+        'ra', 'dec', 'scatter_SN']
+    for par in parameters:
+        results[par] = []
+
+    for LCi, LCfile in enumerate(LCsample):
+
+        print(LCfile)
+        try:
+            resLC = pickle.load(open(LCfile, 'rb'))
+        except EOFError:
+            # The file is empty
+            continue
+
+        if len(resLC) > 0:
+            header = resLC[-1]
+            # If header is None, a bunch of -999 will be output
+        else:
+            # Light curve was discarded
+            continue
+
+        if len(resLC) > 1 and get_lcs_without_flares:
+            continue
+        elif len(resLC) > 1 and not get_lcs_without_flares:
+            pass
+        elif len(resLC) == 1 and get_lcs_without_flares:
+            try:
+                filename = LCfile.split('/')[-1].split('_results')[0]
+            except IndexError:
+                continue
+            lcs_without_flares.append(header)
+            count_lcs_without_flares += 1
+
+        for fi, flare in enumerate(resLC[:-1]):
+            try:
+                for n in np.arange(flare.npeaks):
+                    duration = flare.durations[n].to(units.min).value
+                    if type(duration) == np.ndarray:
+                        results['Duration [min]'].append( \
+                                flare.durations[n].to(units.min).value[3])
+                    else:
+                        # Somehow fit failed
+                        continue
+                    # Identify LC, to later compute flare rates
+                    results['LCname'].append( \
+                            LCfile.split('/')[-1].split('_results.pic')[0])
+                    # Identify flare event number for given LC
+                    results['n_event'].append(fi)
+                    results['redchi2'].append(flare.result_nodip.redchi)
+                    tt, thisflare = flare.get_single_profile(n)
+                    results['Peak time'].append(flare.tref \
+                        + flare.result_nodip.params['tpeak' + str(n)].value)
+                    results['total_duration'].append( \
+                            flare.tend - flare.tbeg)
+                    results['Peak amplitude'].append(thisflare.max())
+                    results['FWHM [min]'].append(24.*60* \
+                        flare.result_nodip.params_LM['fwhm' + str(n)].value)
+                    results['ED [s]'].append(flare.EDs[n].value)
+                    try:
+                        flare.Tstar = header['TEFF']*units.K
+                        flare.Rstar = header['RADIUS']*constants.R_sun
+                        flare.Tflare = 9000.*units.K
+                        flare.wth = wth*units.AA
+                        flare.fth = fth
+                        results['Energy (Shibayama) [erg]'].append( \
+                                    flare.energies[n].value)
+                        results['Teff [K]'].append(header['TEFF'])
+                        results['radius'].append(header['RADIUS'])
+                        #results['phot_var'].append(header['phot_var'].values[0])
+                    except TypeError:
+                        results['Energy (Shibayama) [erg]'].append(-999.)
+                        results['Teff [K]'].append(-999.)
+                        results['radius'].append(-999.)
+                        results['phot_var'].append(-999.)
+                    except IndexError:
+                        # This can only happen for the phot_var info
+                        results['phot_var'].append(-999.)
+
+                    # Stellar params
+                    try:
+                        results['logg'].append(header['LOGG'])
+                        results['ra'].append(header['RA_OBJ'])
+                        results['dec'].append(header['DEC_OBJ'])
+                        results['ticname'].append(header['OBJECT'])
+                        results['tessmag'].append(header['TESSMAG'])
+                    except TypeError:
+                        results['logg'].append(-999.)
+                        results['ra'].append(-999.)
+                        results['dec'].append(-999.)
+                        results['ticname'].append(-999.)
+                        results['tessmag'].append(-999.)
+                    # If possible, compute energy with Davenport's method
+                    #tgd = 'TIC ' + df_stassun['TIC'].astype('string') \
+                    #        == header['OBJECT']
+                    #if np.sum(tgd) > 0:
+                    #    distance = df_stassun['Dist'][tgd].values[0] \
+                    #            *units.parsec
+                    #    mag = header['TESSMAG']
+                    #    flare.get_stellar_luminosity('TESS', mag, distance)
+                    #    results['Peak luminosity [erg s$^{-1}$]'].append( \
+                    #        flare.stellar_luminosity.value*thisflare.max())
+                    #    results['Energy (Davenport) [erg]'].append( \
+                    #            flare.get_flare_energy(n, \
+                    #            method='davenport').value)
+                    #else:
+                    results['Energy (Davenport) [erg]'].append(-999.)
+                    results['Peak luminosity [erg s$^{-1}$]'].append(-999.)
+                    try:
+                        results['Prot'].append(header['Prot_[days]'])
+                        results['FAP'].append(header['FAP'])
+                        results['scatter_SN'].append(header['scatter_SN'])
+                    except TypeError:
+                        results['Prot'].append(-999.)
+                        results['FAP'].append(-999.)
+                        results['scatter_SN'].append(-999.)
+
+                if flare.npeaks > 1:
+                    # See what would happen with a single-flare fit
+                    fcopy = copy.deepcopy(flare)
+                    fcopy.result_nodip.params = copy.deepcopy( \
+                            flare.QPP_candidate['oneflare_model'].params)
+                    fcopy.get_single_duration(0)
+                    #results_oneflare['']
+            except AttributeError as ae:
+                errmsg = 'OSError: No SIMPLE card found, this file does ' \
+                        + 'not appear to be a valid FITS file.'
+                if errmsg in str(ae):
+                    continue
+
+    if not get_lcs_without_flares:
+        for p in parameters:
+            results[p] = np.array(results[p])
+        df = pd.DataFrame.from_dict(results)
+        if get_csv:
+            df.to_csv(result_path + 'results_table.csv', index=False)
+        else:
+            return df
+    else:
+        fout = open(resfolder + 'lcs_without_flares.pic', 'wb')
+        pickle.dump(lcs_without_flares, fout)
+        fout.close()
+
+    return
---
+++ injection_tests.py
@@ -0,0 +1,214 @@
+'''
+Generate a set of light curves with various activity and noise levels,
+and run a flare search on them. Then, compare input and output.
+'''
+
+import celerite2
+from celerite2 import terms
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import CubicSpline
+from scipy.stats import loguniform
+import itertools
+from models import flare_model_mendoza
+import glob
+import pickle
+from fladerunner import analyse_LC, get_results
+from astropy import units
+from pdb import set_trace
+
+def model_LC(sigma, Prot, Q0, dQ, noise, f=0.5, tmax=28., deltat=20./86400., \
+            plots=False):
+    '''
+    By default, assume a 28 days TESS-duration light curve with 20-s cadence.
+
+    Parameters
+    ----------
+    nsample: number of samples to draw from the generated GP.
+    '''
+    t_gp = np.arange(0., tmax, deltat)[::100]
+
+    # Quasi-periodic term
+    kernel = terms.RotationTerm(sigma=sigma, period=Prot, Q0=Q0, dQ=dQ, f=f)
+    gp = celerite2.GaussianProcess(kernel, mean=1.0)
+    gp.compute(t_gp)
+    mu = gp.sample(size=1)
+
+    # Increase time cadence
+    interp = CubicSpline(t_gp, mu[0])
+    t = np.arange(0., tmax, deltat)
+    y = interp(t)
+
+    y_scatter = y*np.random.normal(loc=1., scale=noise, size=len(t))
+
+    if plots:
+        for i in range(np.shape(mu)[0]):
+            plt.plot(t, y_scatter, '.')
+            plt.plot(t, y, linewidth=3)
+        plt.show()
+        set_trace()
+        plt.close()
+
+    return t, y_scatter
+
+def generate_LCs(folder_out, plots=False):
+    '''
+    Generate a grid of input parameters for the GP.
+    '''
+    # Stellar parameter grid
+    sigma = np.logspace(-3, -1.3, 10)
+    Prot = np.linspace(0.5, 10., 5)
+    Q0 = np.linspace(0.1, 1., 3)
+    dQ = np.linspace(0.5, 1.5, 3)
+    # Scale TESS's noise level for 1 hr to 20 sec
+    noise_range = 1e-6*np.linspace(50., 1000., 5)*(60.*3.)**0.5
+
+    nLC = len(sigma)*len(Prot)*len(Q0)*len(dQ)*len(noise_range)
+    print('Generating', nLC, 'light curves...')
+
+    for c, i in enumerate(itertools.product(sigma, Prot, Q0, dQ, noise_range)):
+        if c > 100:
+            continue
+        print(c)
+        sigma_i, Prot_i, Q0_i, dQ_i, noise_i = i
+        t, y_quiet = model_LC(sigma_i, Prot_i, Q0_i, dQ_i, noise_i)
+        # Flare parameter grid
+        flares_per_lc = np.random.randint(low=10, high=100)
+        tpeak = np.random.uniform(low=0., high=28., size=flares_per_lc)
+        fwhm = loguniform.rvs(a=20./86400., b=1./24., size=flares_per_lc)
+        ampl = loguniform.rvs(a=1e-4, b=0.1, size=flares_per_lc)
+
+        y = np.copy(y_quiet)
+        for j in range(flares_per_lc):
+            par = {}
+            par['tpeak0'] = tpeak[j]
+            par['fwhm0'] = fwhm[j]
+            par['ampl0'] = ampl[j]
+            par['D20'] = 1.2151
+            y += flare_model_mendoza(t, par)
+
+        # Save time series and input pars
+        LC = {}
+        LC['stellar_pars'] = {}
+        LC['stellar_pars']['meta'] = ['sigma, Prot, Q0, dQ, noise']
+        LC['stellar_pars']['values'] = i
+        LC['flare_pars'] = {}
+        LC['flare_pars']['meta'] = ['tpeak', 'fwhm', 'ampl']
+        LC['flare_pars']['values'] = [tpeak, fwhm, ampl]
+        LC['t'] = {}
+        LC['t']['meta'] = ['tini', 'tmax', 'deltat']
+        LC['t']['values'] = [t.min(), t.max() + np.diff(t)[0], np.diff(t)[0]]
+        LC['y'] = y
+
+        fout = open(folder_out + 'LC_' + str(c) + '.pic', 'wb')
+        pickle.dump(LC, fout)
+        fout.close()
+
+        if plots:
+            plt.plot(t, y)
+            plt.show()
+            set_trace()
+            plt.close()
+
+    return
+
+def analyse_LCs(folderin):
+
+    plt.ioff()
+
+    saveresfolder = folderin.replace('simulated_LCs', 'analysis')
+
+    datadir = '/home/giovanni/Projects/data'
+    throughput_folder = datadir + '/filters/'
+    wth, fth = np.loadtxt(throughput_folder + 'TESS_TESS.Red.dat', unpack=True)
+
+    LCs = glob.glob(folderin + '*pic')
+    LCs = sorted(LCs)
+    print('Analysing', len(LCs), 'simulated light curves...')
+    for LC_i, LC in enumerate(LCs):
+        print(LC_i)
+        data = pickle.load(open(LC, 'rb'))
+        tv = data['t']['values']
+        t = np.arange(tv[0], tv[1], tv[2])
+        y = data['y']
+        yerr = np.zeros(len(t)) + data['stellar_pars']['values'][-1]
+
+        saveresfile = saveresfolder + LC.split('/')[-1]
+        fout_str = saveresfile.replace('.pic', '_results.pic')
+
+        flarespar, flaresflag = analyse_LC([t, y, yerr], saveresfile, wth*units.AA, fth)
+
+        fout = open(fout_str, 'wb')
+        pickle.dump(flarespar, fout)
+        fout.close()
+
+    return
+
+def get_simulation_results(folderin):
+    '''
+    Work one LCs at a time and plot results for matching flares.
+    '''
+    LCs = glob.glob(folderin + '*pic')
+    LCs = sorted(LCs)
+
+    fig1, ax1 = plt.subplots()
+    fig2, ax2 = plt.subplots()
+    for LC in LCs:
+        data_in = pickle.load(open(LC, 'rb'))
+        tpeaks_in = data_in['flare_pars']['values'][0]
+        fwhm_in = data_in['flare_pars']['values'][1]
+        ampl_in = data_in['flare_pars']['values'][2]
+
+        data_out = LC.replace('simulated_LCs', 'analysis').replace('.pic', \
+                    '_results.pic')
+        try:
+            df = get_results([data_out], folderin + '..', \
+                    get_lcs_without_flares=False, get_csv=False)
+        except FileNotFoundError:
+            continue
+
+        pairs = find_closest_pair(df['Peak time'], tpeaks_in, 5)
+        index_output = [x[0] for x in pairs]
+        index_input = [x[1] for x in pairs]
+
+        ax1.scatter(ampl_in[index_input], \
+            df.iloc[index_output]['Peak amplitude'], marker='.', color='k')
+        ax2.scatter(fwhm_in[index_input]*24*60., \
+            df.iloc[index_output]['FWHM [min]'], marker='.', color='k')
+
+    xx = np.logspace(-3, -1, 1000)
+    ax1.plot(xx, xx, 'r')
+    xx2 = np.logspace(-0.5, 2, 1000)
+    ax2.plot(xx2, xx2, 'r')
+
+    ax1.set_xlabel('Injected peak amplitude', fontsize=14)
+    ax1.set_ylabel('Retrieved peak amplitude', fontsize=14)
+    ax1.set_xscale('log')
+    ax1.set_yscale('log')
+    ax2.set_xlabel('Injected FWHM [min]', fontsize=14)
+    ax2.set_ylabel('Retrieved FWHM [min]', fontsize=14)
+    ax2.set_xscale('log')
+    ax2.set_yscale('log')
+    plt.show()
+    set_trace()
+
+    return
+
+def find_closest_pair(arr1, arr2, tolerance):
+    '''
+    Return indices of arr1 and arr2 that are closer than tolerance data ponints.
+    Only the closest pairs are provided.
+    '''
+    arr1 = np.asarray(arr1)
+    arr2 = np.asarray(arr2)
+
+    pairs = []
+
+    for arr1_idx, a in enumerate(arr1):
+        diffs = np.abs(arr2 - a)
+        arr2_min_idx = np.argmin(diffs)
+        if diffs[arr2_min_idx] <= tolerance:
+            pairs.append((arr1_idx, arr2_min_idx))
+        # else: no match within tolerance
+
+    return pairs
